94ad377aed8b789a1abe39cd91452ed0cc21fe44
Mike Funk <mike@mikefunk.com>
2015-10-28 10:00:02 -0700

    updates

--- install/05_vim_config_install.sh
+++ install/05_vim_config_install.sh
@@ -46,9 +46,16 @@ log_info "installing vim-plug"
 log_info "linking UltiSnips custom snippets dir"
 link_this "$HOME/.dotfiles/to_link/UltiSnips" "$HOME/.vim/UltiSnips"
 
+# neovim config symlinks
 log_info "linking configs for neovim"
-link_this "$HOME/.vim" "$HOME/.nvim"
-link_this "$HOME/.vimrc" "$HOME/.nvimrc"
+# old style
+# link_this "$HOME/.vim" "$HOME/.nvim"
+# link_this "$HOME/.vimrc" "$HOME/.nvimrc"
+
+# new style
+[ -d "$HOME/.config/nvim" ] || mkdir -p "$HOME/.config/nvim" && log_info "~/.config/nvim directory created."
+link_this "$HOME/.dotfiles/to_link/vim/.vimrc" "$HOME/.config/nvim/init.vim"
+link_this "$HOME/.vim" "$HOME/.config/nvim"
 
 # I just can't seem to get this working
 # log_info "linking nerdtree plugins"
