5bc96305cf56f214457612fad218e8ea8468d211
Mike Funk <mike@mikefunk.com>
2018-04-05 14:17:35 -0700

    working

--- .support/install-pear.sh
+++ .support/install-pear.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/expect
-spawn wget -O $HOME/.phpenv/bin/go-pear.phar http://pear.php.net/go-pear.phar
+# TODO get relative path... not as easy as it sounds
+spawn wget -O "/Users/mikefunk/.phpenv/bin/go-pear.phar" http://pear.php.net/go-pear.phar
 expect eof
 
-spawn php $HOME/.phpenv/bin/go-pear.phar
+spawn php "/Users/mikefunk/.phpenv/bin/go-pear.phar"
 
 expect "1-11, 'all' or Enter to continue:"
 send "1\r"
-send "$HOME/.phpenv/pear\r"
+send "/Users/mikefunk/.phpenv/pear\r"
 send "\r"
 expect eof
