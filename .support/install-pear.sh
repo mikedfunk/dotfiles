a7c324d02bbfa7f2e7ebb7904614829e283daa4f
Mike Funk <mike@mikefunk.com>
2018-04-05 23:40:45 -0700

    working

--- .support/install-pear.sh
+++ .support/install-pear.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/expect
 spawn php ".phpenv/bin/go-pear.phar"
 
-expect "1-11, 'all' or Enter to continue:"
+expect "1-12, 'all' or Enter to continue:"
 send "1\r"
 # TODO get absolute path to relative path... not as easy as it sounds
 send "/Users/mikefunk/.phpenv/pear\r"
+expect "1-12, 'all' or Enter to continue:"
 # expect "Would you like to alter php.ini </Users/mikefunk/.phpenv/versions/7.0.29/etc/php.ini>? [Y/n] :"
 # send "Y\r"
 send "\r"
