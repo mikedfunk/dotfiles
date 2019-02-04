1b511a66fb382292218ebf9eadd2688ca116ffa1
Mike Funk <mike@mikefunk.com>
2019-02-04 13:28:44 -0800

    working

--- Code/saatchi/saatchiart/phpstan_bootstrap.php
+++ Code/saatchi/saatchiart/phpstan_bootstrap.php
@@ -1,4 +1,6 @@
 <?php
 define('APPLICATION_ENV', 'local');
 define('APPLICATION_PATH', '.');
+define('DHL_URL', '');
+define('OCI_DEFAULT', '');
 set_include_path(get_include_path() . PATH_SEPARATOR . 'library');
