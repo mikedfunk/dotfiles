f60f18307150a61035a9b1e2472f1d54d397aa76
Mike Funk <mike@mikefunk.com>
2020-07-27 12:07:32 -0700

    working

--- .support/circleci-notify.sh
+++ .support/circleci-notify.sh
@@ -13,8 +13,11 @@ USERNAME=$1
 PROJECT=$2
 BRANCH=$3
 
-function uriencode { jq -nr --arg v "$1" '$v|@uri'; }
-BRANCH_ENCODED=$(uriencode "${BRANCH}")
+function uriencode () {
+    jq -nr --arg v "$1" '$v|@uri';
+}
+
+BRANCH_ENCODED=$(uriencode "$BRANCH")
 IS_FINISHED=0
 
 function notify () {
@@ -31,39 +34,35 @@ function notify () {
 }
 
 function check_result () {
-    # echo "http --check-status GET https://circleci.com/api/v1.1/project/github/${USERNAME}/${PROJECT}/tree/${BRANCH_ENCODED}?circle-token=${TOKEN}&limit=1"
     RESULT=$(http --check-status GET "https://circleci.com/api/v1.1/project/github/${USERNAME}/${PROJECT}/tree/${BRANCH_ENCODED}?circle-token=${TOKEN}&limit=1")
     if [[ $? != 0 ]]; then
-        # echo "Non-200 response code received"
-        # NOTI_NSUSER_SOUNDNAME=ping /usr/local/bin/noti --title "❌ CI: ${PROJECT} ${BRANCH}" --message "CircleCI API check failed"
-        notify "❌ CI: ${PROJECT} ${BRANCH}" "CircleCI API check failed" basso
+        notify "❌ CI: $PROJECT $BRANCH" "CircleCI API check failed" basso
         IS_FINISHED=1
         return 1
     fi
-    FIRST_BUILD=$(echo ${RESULT} | jq '.[0]')
-    if [[ "${FIRST_BUILD}" == 'null' ]]; then
-        # echo "No builds found for this branch"
-        notify "❌ CI: ${PROJECT} ${BRANCH}" "No CircleCI builds found for this branch" basso
+    FIRST_BUILD=$(echo $RESULT | jq '.[0]')
+    if [[ "$FIRST_BUILD" == 'null' ]]; then
+        notify "❌ CI: $PROJECT $BRANCH" "No CircleCI builds found for this branch" basso
         IS_FINISHED=1
         return 1
     fi
-    STATUS=$(echo ${RESULT} | jq '.[0].status') # e.g. success or failed
-    BUILD_URL=$(echo ${RESULT} | jq '.[0].build_url')
-    LIFECYCLE=$(echo ${RESULT} | jq '.[0].lifecycle') # e.g. finished
-    if [[ "${LIFECYCLE}" == '"finished"' ]]; then
+    STATUS=$(echo $RESULT | jq '.[0].status') # e.g. success or failed
+    BUILD_URL=$(echo $RESULT | jq '.[0].build_url')
+    LIFECYCLE=$(echo $RESULT | jq '.[0].lifecycle') # e.g. finished
+    if [[ "$LIFECYCLE" == '"finished"' ]]; then
         IS_FINISHED=1
     else
         # echo "Lifecycle is not yet finished"
         return
     fi
-    if [[ "${STATUS}" == '"success"' ]]; then
-        "✅ CI: ${PROJECT} ${BRANCH}" "CI passed in CircleCI" glass "${BUILD_URL}"
-    elif [[ "${STATUS}" == '"failed"' ]]; then
-        "❌ CI: ${PROJECT} ${BRANCH}" "CI failed in CircleCI" basso "${BUILD_URL}"
+    if [[ "$STATUS" == '"success"' ]]; then
+        "✅ CI: $PROJECT $BRANCH" "CI passed in CircleCI" glass "$BUILD_URL"
+    elif [[ "$STATUS" == '"failed"' ]]; then
+        "❌ CI: $PROJECT $BRANCH" "CI failed in CircleCI" basso "$BUILD_URL"
     fi
 }
 
-while [[ "${IS_FINISHED}" == 0 ]]; do
+while [[ "$IS_FINISHED" == 0 ]]; do
     sleep 30
     check_result
 done
