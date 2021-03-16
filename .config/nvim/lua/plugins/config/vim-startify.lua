583ded6621a7792c06670cc866b70a365c57e38a
Mike Funk <mike@mikefunk.com>
2021-03-15 23:22:49 -0700

    working

--- .config/nvim/lua/plugins/config/vim-startify.lua
+++ .config/nvim/lua/plugins/config/vim-startify.lua
@@ -1,8 +1,8 @@
-local g, getcwd, getenv = vim.g, vim.fn.getcwd, vim.fn.getenv
+local g, getcwd, getenv, nvim_set_keymap = vim.g, vim.fn.getcwd, vim.fn.getenv, vim.api.nvim_set_keymap
 
 g['ascii'] = {}
 g['startify_custom_header'] = 'map(g:ascii + startify#fortune#boxed(), "\\"   \\".v:val")'
-g['startify_bookmarks'] = {'~/.vimrc', '~/.vimrc.plugins'}
+-- g['startify_bookmarks'] = {'~/.config/nvim/init.lua', '~/.zshrc', '~/.tmux.conf', '~/.saatchirc'}
 
 g['startify_enable_special'] = 0 -- show empty buffer and quit
 g['startify_enable_unsafe'] = 1 -- speeds up startify but sacrifices some accuracy
@@ -10,12 +10,16 @@ g['startify_session_sort'] = 1 -- sort descending by last used
 g['startify_skiplist'] = {'COMMIT_EDITMSG', '.DS_Store'} -- disable common but unimportant files
 g['startify_files_number'] = 9 -- recently used
 g['startify_session_persistence'] = 0 -- auto save session on exit like obsession
-g['startify_session_dir'] = getenv('HOME') .. '/.vim/sessions' .. getcwd() -- gitsessions dir... but actually it's a pretty good idea to use even outside of gitsessions.
+g['startify_session_dir'] = getenv('HOME') .. '/.local/share/nvim/session' .. getcwd() -- session dir for each repo
 
 -- reorder and whitelist certain groups
 g['startify_lists'] = {
   {type = 'sessions', header = {'   Sessions'}},
-  {type = 'dir', header = {'   MRU ' .. getcwd()}},
+  {type = 'dir', header = {'   Recent in ' .. getcwd()}},
   {type = 'commands', header = {'   Commands'}},
 }
 g['startify_change_to_dir'] = 0 -- this feature should not even exist. It is stupid.
+
+nvim_set_keymap('n', '<leader>ss', ':SSave<cr>', {noremap = true}) -- disable Ex mode
+nvim_set_keymap('n', '<leader>sl', ':SLoad<cr>', {noremap = true}) -- disable Ex mode
+nvim_set_keymap('n', '<leader>sd', ':SDelete<cr>', {noremap = true}) -- disable Ex mode
